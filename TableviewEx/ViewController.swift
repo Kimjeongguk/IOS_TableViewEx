0b823bccb7f5a56814cd95eabc3335ac4224b39a
KimJeongguk <mindstar7949@naver.com>
2021-08-25 18:06:24 +0900

    Initial Commit

---
+++ TableviewEx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableviewEx
+//
+//  Created by Jeongguk Kim on 2021/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
